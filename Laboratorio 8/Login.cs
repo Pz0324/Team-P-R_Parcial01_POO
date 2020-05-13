79a75d5f4e36d651be334d77ff7910b3e1e107b3
Pz0324 <00062019@uca.edu.sv>
2020-05-13 14:52:10 -0600

    ADD

---
+++ Laboratorio 8/Login.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Laboratorio_8
+{
+    public partial class Login : UserControl
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Laboratorio 8/Register.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Laboratorio_8
+{
+    public partial class Register : UserControl
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+    }
+}
